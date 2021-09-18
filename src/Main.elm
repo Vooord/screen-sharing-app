11ffaed0da6df0a6bf7db2c3802f1c9a61e3ae3b
Vooord <yan.novak.2014@gmail.com>
2021-09-18 23:09:17 +0300

    Add sharing config loading

--- src/Main.elm
+++ src/Main.elm
@@ -3,20 +3,40 @@ module Main exposing (..)
 import Browser
 import Html exposing (..)
 import Http
-import Bytes.Decode as DecodeBytes
+import Json.Decode as DecodeJson exposing (Decoder)
+
 
 
 ---- MODEL ----
 
 
-type Role =
-    Speaker |
-    Participant
+type SpeakerRole
+    = Speaker
 
-type Model =
-    LoadingRole |
-    RoleDone { role: Role } |
-    RoleFailed
+
+type ParticipantRole
+    = Participant
+
+
+type Role
+    = SpeakerRole
+    | ParticipantRole
+
+
+type alias ParticipantRoleData =
+    { role : ParticipantRole }
+
+
+type alias SpeakerRoleData =
+    { role : SpeakerRole }
+
+
+type Model
+    = LoadingRole
+    | RoleFailed
+    | LoadingSharingConfig SpeakerRoleData
+    | WaitingForTranslation ParticipantRoleData
+    | WaitingForStartSharing SpeakerRoleData (List String)
 
 
 init : ( Model, Cmd Msg )
@@ -30,13 +50,47 @@ init =
 
 type Msg
     = GotRole (Result Http.Error Int)
+    | GotSharingConfig (Result Http.Error (List String))
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        GotRole (Ok role) -> (RoleDone { role = if role == 1 then Speaker else Participant }, Cmd.none)
-        GotRole (Err _) -> (RoleFailed, Cmd.none)
+        GotRole (Ok roleBit) ->
+            handleGotRole roleBit
+
+        GotRole (Err _) ->
+            ( RoleFailed, Cmd.none )
+
+        GotSharingConfig (Ok sharingConfig) ->
+            case model of
+                LoadingSharingConfig roleData ->
+                    ( WaitingForStartSharing roleData sharingConfig, Cmd.none )
+
+                _ ->
+                    ( model, Cmd.none )
+
+        GotSharingConfig (Err _) ->
+            ( model, Cmd.none )
+
+
+handleGotRole : Int -> ( Model, Cmd Msg )
+handleGotRole roleBit =
+    let
+        isSpeaker =
+            determineRole roleBit == SpeakerRole
+    in
+    ( if isSpeaker then
+        LoadingSharingConfig { role = Speaker }
+
+      else
+        WaitingForTranslation { role = Participant }
+    , if isSpeaker then
+        requestSharingConfig
+
+      else
+        Cmd.none
+    )
 
 
 
@@ -48,10 +102,65 @@ view model =
     case model of
         LoadingRole ->
             div [] [ text "Loading role..." ]
-        RoleDone { role } ->
-            div [] [ text ("Great! You're " ++ if role == Speaker then "participant" else "speaker") ]
+
         RoleFailed ->
             div [] [ text "Failed to load role :(" ]
+
+        LoadingSharingConfig _ ->
+            div [] [ text "Loading sharing config..." ]
+
+        WaitingForTranslation _ ->
+            -- TODO: make canvas
+            div [] [ text "Waiting for translation..." ]
+
+        WaitingForStartSharing _ _ ->
+            div [] [ button [] [ text "Start" ] ]
+
+
+
+-- API --
+
+
+apiHost : String
+apiHost =
+    "https://vord1-elm.free.beeceptor.com"
+
+
+buildApiPath : String -> String
+buildApiPath path =
+    apiHost ++ "/" ++ path
+
+
+requestRole : Cmd Msg
+requestRole =
+    Http.get
+        { url = buildApiPath "current_role"
+
+        --    it's might be better to use
+        --    expect = Http.expectBytes GotRole DecodeBytes.unsignedInt8
+        , expect = Http.expectJson GotRole DecodeJson.int
+        }
+
+
+requestSharingConfig : Cmd Msg
+requestSharingConfig =
+    Http.get
+        { url = buildApiPath "sharing_config"
+        , expect = Http.expectJson GotSharingConfig (DecodeJson.list DecodeJson.string)
+        }
+
+
+
+-- utils --
+
+
+determineRole : Int -> Role
+determineRole roleBit =
+    if roleBit == 1 then
+        SpeakerRole
+
+    else
+        ParticipantRole
 
 
 
@@ -66,19 +175,3 @@ main =
         , update = update
         , subscriptions = always Sub.none
         }
-
-
--- API --
-
-apiHost: String
-apiHost =
-    "https://vord-elm.free.beeceptor.com"
-
-
-requestRole: Cmd Msg
-requestRole =
-    Http.get {
-    url = apiHost ++ "/current_role",
-    expect = Http.expectBytes GotRole DecodeBytes.unsignedInt8
-    }
-
